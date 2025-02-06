495c78c37545d14f74bcffd2a41a073286c89c6a
daridev <darideveloper@gmail.com>
2025-02-06 16:05:28 -0600

    feat (models): add id to all models

--- chatbot_app/models.py
+++ chatbot_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class TipoLead(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
 
     def __str__(self):
@@ -9,6 +10,7 @@ class TipoLead(models.Model):
 
 
 class Programa(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     tipo_lead = models.ForeignKey(TipoLead, on_delete=models.CASCADE)
 
@@ -17,6 +19,7 @@ class Programa(models.Model):
 
 
 class Momento(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     respuesta_momento = models.TextField(null=True, blank=True)
     programa = models.ForeignKey(Programa, on_delete=models.CASCADE)
@@ -26,6 +29,7 @@ class Momento(models.Model):
 
 
 class Submomento(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     momento = models.ForeignKey(Momento, on_delete=models.CASCADE)
     programa = models.ForeignKey(
@@ -38,24 +42,17 @@ class Submomento(models.Model):
 
 
 class Respuesta(models.Model):
+    id = models.AutoField(primary_key=True)
     contenido = models.TextField()
     prioridad = models.IntegerField()
     submomento = models.ForeignKey(Submomento, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.contenido
-
-
-# class Historial(models.Model):
-#     momento = models.ForeignKey(Momento, on_delete=models.CASCADE, default=1)  # Cambia '1' por el ID de un momento existente
-#     submomento = models.ForeignKey(Submomento, null=True, blank=True, on_delete=models.CASCADE)
-#     respuesta = models.ForeignKey(Respuesta, null=True, blank=True, on_delete=models.CASCADE)
-#     timestamp = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return f"{self.momento.nombre} - {self.timestamp}"
+    
 
 class Documento(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=200)
     archivo = models.FileField(upload_to='documentos/')
     palabras_clave = models.TextField()
--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -9,7 +9,7 @@ router = routers.DefaultRouter()
 
 router.register(r'tipolead', views.TipoLeadViewSet, 'tipolead')
 router.register(r'programa', views.ProgramaViewSet, 'programa')
-router.register(r'momento',  views.MomentoViewSet, 'momento')
+router.register(r'momento', views.MomentoViewSet, 'momento')
 router.register(r'submomento', views.SubmomentoViewSet, 'submomento')
 router.register(r'respuesta', views.RespuestaViewSet, 'respuesta')
 router.register(r'documento', views.DocumentoViewSet, 'documento')
