a9f52ed1aaa4fa4fcbd54ec7b96dc336de3f2ff7
daridev <darideveloper@gmail.com>
2025-02-26 16:22:32 -0600

    fix (project/url): setup media root link

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include  # Asegúrate de importar `include`
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # Redirects
@@ -14,3 +17,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chatbot_app.urls')),  # Redirige al `chatbot_app`
 ]
+
+if not settings.STORAGE_AWS:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
